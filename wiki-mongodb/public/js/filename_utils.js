4191b0689511fab3ee8f76db3ead35fba2e70569
Hugo J. Bello <hugo.bello.gu@gmail.com>
2017-09-11 20:55:39 +0200

    editor now handles new pages

--- wiki-mongodb/public/js/filename_utils.js
+++ wiki-mongodb/public/js/filename_utils.js
@@ -1,5 +1,5 @@
 function titleToFilename(title){
   var name = '';
-  name=title.replace(' ','_').replace('á','a').replace('é','e').replace('í','i').replace('ó','o').replace('ú','u').replace('ñ','n');
+  name=title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
   return name;
 }
