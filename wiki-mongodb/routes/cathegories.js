13ad75a5e8b26d1e391471f788b3372056912e58
Hugo J. Bello <hugo.bello.gu@gmail.com>
2017-09-16 00:08:14 +0200

    cathegories and labbels added

--- wiki-mongodb/routes/cathegories.js
+++ wiki-mongodb/routes/cathegories.js
@@ -7,6 +7,7 @@ router.get('/cathegory/:cathegory_id', function(req, res) {
     console.log(req.params.cathegory_id);
     Cathegory.findOne({'_id':req.params.cathegory_id}, function(err, cathegory){
       if (err) throw err;
+      console.log(cathegory);
       if (!cathegory){
         return res.redirect('/category_editor/'+ req.params.cathegory_id);
       } else {
@@ -20,6 +21,8 @@ router.get('/cathegory/:cathegory_id', function(req, res) {
 router.get('/cathegory_editor/:cathegory_id', function(req, res) {
     Cathegory.findOne({'_id':req.params.cathegory_id}, function(err, cathegory){
       if (err) throw err;
+      console.log(cathegory);
+
       if (!cathegory){
         return res.render('cathegory_editor', {cathegory_id :  req.params.cathegory_id, user : req.user});
       } else {
@@ -27,5 +30,19 @@ router.get('/cathegory_editor/:cathegory_id', function(req, res) {
       }
     });
 });
-
+router.post('/cathegory_editor', function(req, res) {
+  var cathegory = new Cathegory({'_id': req.body._id,
+                            'name':req.body.cathegory_name,
+                            'description':req.body.description,
+                            'modified_at': new Date()});
+  if (req.body.new =='true'){
+    entry.created_at = new Date();
+    Cathegory.create(cathegory, function(err,raw){
+      if (err) throw err;
+    return res.redirect('/cathegory/'+req.body._id);
+    });
+  } else  {
+    Cathegory.findByIdAndUpdate(req.body._id, cathegory, function(err,raw){
+      if (err) throw err;
+      return res.redirect('/cathegory/'+req.body._id);    });
 module.exports = router;
